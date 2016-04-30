dc2fee675e4779276e46182a40cbd11c00a829c9
Brandon Cercone <bcerco@gmail.com>
2016-04-30 17:34:07 -0400

    This should work properly now.

--- pmpi_distribute_source.c
+++ pmpi_distribute_source.c
@@ -11,20 +11,40 @@ void
 pmpi_distribute_source(int nodes, char *source)
 {
 	int count = 0;
-	FILE *f;
+	FILE *f_host;
+	FILE *f_node;
 	size_t len = 0;
 	ssize_t read;
-	char *line = NULL;
-	f = fopen("hosts.txt", "r");
-	if (f == NULL)
+	char * line = NULL;
+	char * node_addr = NULL;
+	
+	/* open the file that contains this nodes IP address */
+	f_node = fopen("node_add.txt", "r");
+	if (f_node == NULL){
+		fprintf(stderr, "Error opening node_add.txt --> %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
-	while((read = getline(&line, &len, f)) != -1
-			&& count < nodes){
+	}
+	/* get the address of the node calling this function */
+	while ((read = getline(&node_addr, &len, f_node)) != -1){
+		node_addr[strlen(node_addr) -1] = '\0';
+	}
+	fclose(f_node);
+	len = 0;
+	
+	/* open the file that contains the addresses of all nodes */
+	f_host = fopen("hosts.txt", "r");
+	if (f_host == NULL){
+		fprintf(stderr, "Error opening host.txt --> %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	/* send source code to other n-1 nodes whose IP is not the same as the one 
+	 * calling this function */
+	while ((read = getline(&line, &len, f_host)) != -1 && count < nodes) {
 		line[strlen(line) -1] = '\0';
-		if (strcmp(line, master) != 0){
+		if (strcmp(line, node_addr) != 0){
 			pmpi_send_source(line, source);
 			count++;
 		}
 	}
-	fclose(f);
+	fclose(f_host);
 }
